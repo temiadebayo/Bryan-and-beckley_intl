9e7bcb9c6cf09e36c68f721a941468766aa86ff9
bryan <obraye05@gmail.com>
2023-12-15 15:07:36 +0100

    removing Team from Navbar

--- components/About.js
+++ components/About.js
@@ -6,10 +6,10 @@ const About = () => {
       <section className="text-lg scroll-smooth">
         <div className="md:py-[150px] bg-[#4A4599] py-[80px]">
           <div>
-            <h1 className="px-2 text-[50px] text-center font-semibold leading-[2.9rem] text-[#FFA9FF]">
+            <h1 className="px-2 text-[30px] md:text-[50px] text-center font-semibold leading-[2.9rem] text-[#FFA9FF]">
               Redefining the future Of Distribution Patternship
             </h1>
-            <p className=" text-left max-w-2xl md:mt-7 font-white mx-auto px-2 text-[30px] md:text-center text-gray-50 font-regular leading-9 my-5">
+            <p className=" text-left max-w-2xl md:mt-6 font-white mx-auto px-2 sm:text-[30px] text-[20px] md:text-center text-gray-50 font-regular leading-8 py-5">
               We are committed to building cloud-based and client-centered
               technology solutions that will transform the distribution
               experience for everyone.
@@ -20,7 +20,7 @@ const About = () => {
               {/* Our Mission */}
               <div className="bg-white p-8 shadow-md max-w-2xl">
                 <hr className="w-8 mb-2 border-t-8 border-[#4A4599]"></hr>
-                <h2 className="text-3xl tablet:text-4xl font-semibold mb-2">
+                <h2 className="text-3xl md:text-4xl font-semibold mb-2">
                   Our Mission
                 </h2>
                 <div className="max-w-4xl text-[20px] leading-8">
@@ -38,7 +38,7 @@ const About = () => {
               {/* Our Services */}
               <div className="bg-white p-8 shadow-md max-w-2xl">
                 <hr className="w-8 mb-2 border-t-8 border-[#4A4599]"></hr>
-                <h2 className="text-3xl tablet:text-4xl font-semibold mb-2">
+                <h2 className="text-3xl md:text-4xl font-semibold mb-2">
                   Our Services
                 </h2>
                 <div className="max-w-4xl text-[20px] leading-8">
