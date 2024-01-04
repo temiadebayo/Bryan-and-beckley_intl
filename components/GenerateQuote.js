eaaed5804e5ed58333ea90d168b4baeabb3183f7
bryan <obraye05@gmail.com>
2024-01-04 03:31:32 +0100

    Navbar fix

--- components/GenerateQuote.js
+++ components/GenerateQuote.js
@@ -6,8 +6,8 @@ import { CldUploadWidget } from 'next-cloudinary';
 
 const GenerateQuote = () => {
   return (
-    <main className="relative py-28 bg-gray-900">
-      <div className="relative z-10 max-w-screen-xl mx-auto text-gray-600 sm:px-4 md:px-8">
+    <main className=" py-28 bg-gray-900">
+      <div className=" z-10 max-w-screen-xl mx-auto text-gray-600 sm:px-4 md:px-8">
         <div className="max-w-lg space-y-3 px-4 sm:mx-auto sm:text-center sm:px-0">
           <h3 className="text-cyan-400 font-semibold">Contact</h3>
           <p className="text-white text-3xl font-semibold sm:text-4xl">
@@ -77,13 +77,13 @@ const GenerateQuote = () => {
           </form>
         </div>
       </div>
-      <div
-        className="absolute inset-0 blur-[118px] max-w-lg h-[800px] mx-auto sm:max-w-3xl sm:h-[400px]"
+      {/* <div
+        className=" inset-0 blur-[118px] max-w-lg h-[800px] mx-auto sm:max-w-3xl sm:h-[400px]"
         style={{
           background:
             'linear-gradient(106.89deg, rgba(192, 132, 252, 0.11) 15.73%, rgba(14, 165, 233, 0.41) 15.74%, rgba(232, 121, 249, 0.26) 56.49%, rgba(79, 70, 229, 0.4) 115.91%)',
         }}
-      ></div>
+      ></div> */}
     </main>
   );
 };
